fbf921d122635aac464a801f24e80a8c805988b7
MaDDi <vladislav.sl9696@gmail.com>
2022-01-04 17:55:02 +0300

    Add daterangepicker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
+import 'daterangepicker'
+
+require('packs/home');
 
 Rails.start()
 Turbolinks.start()
